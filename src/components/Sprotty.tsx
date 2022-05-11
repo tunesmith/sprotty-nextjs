91e53675a71fc2343b71076110bacfda5accc13b
Curt Siffert <curt@keenworks.com>
2022-05-10 21:02:50 -0700

    Install sprotty, add initial functions from example

--- src/components/Sprotty.tsx
+++ src/components/Sprotty.tsx
@@ -1,4 +1,54 @@
+import { Bounds, Point, SEdge, SModelElement, SNode } from "sprotty-protocol";
+
+const NODE_SIZE = 60;
+
 const Sprotty = () => {
+  let count = 2;
+  function addNode(bounds: Bounds): SModelElement[] {
+    const newNode: SNode = {
+      id: "node" + count,
+      type: "node:circle",
+      position: {
+        x: bounds.x + Math.random() * (bounds.width - NODE_SIZE),
+        y: bounds.y + Math.random() * (bounds.height - NODE_SIZE),
+      },
+      size: {
+        width: NODE_SIZE,
+        height: NODE_SIZE,
+      },
+    };
+    const newEdge: SEdge = {
+      id: "edge" + count,
+      type: "edge:straight",
+      sourceId: "node0",
+      targetId: "node" + count,
+    };
+    count++;
+    return [newNode, newEdge];
+  }
+
+  function focusGraph(): void {
+    const graphElement = document.getElementById("graph");
+    if (graphElement !== null && typeof graphElement.focus === "function")
+      graphElement.focus();
+  }
+
+  function getVisibleBounds({
+    canvasBounds,
+    scroll,
+    zoom,
+  }: {
+    canvasBounds: Bounds;
+    scroll: Point;
+    zoom: number;
+  }): Bounds {
+    return {
+      ...scroll,
+      width: canvasBounds.width / zoom,
+      height: canvasBounds.height / zoom,
+    };
+  }
+
   return (
     <div>
       <h1>Sprotty Circles Example</h1>
