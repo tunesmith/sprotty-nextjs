bb4b9b066794033e7e80e513f84124acfa59c814
Curt Siffert <curt@keenworks.com>
2022-05-10 20:58:51 -0700

    Simplified jsx from example html

--- src/components/Sprotty.tsx
+++ src/components/Sprotty.tsx
@@ -1,5 +1,17 @@
 const Sprotty = () => {
-    return <div>hello</div>
-}
+  return (
+    <div>
+      <h1>Sprotty Circles Example</h1>
+      <p>
+        <button id="addNode">Add node</button>
+        <button id="scrambleAll">Scramble all</button>
+        <button id="scrambleSelection">Scramble selection</button>
+      </p>
+      <div>
+        <div id="sprotty" className="sprotty" />
+      </div>
+    </div>
+  );
+};
 
 export default Sprotty;
